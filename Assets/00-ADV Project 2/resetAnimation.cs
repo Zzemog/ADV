4cc1fb44816db6b287d3fe3176ba9b05b67464b5
JOSE FRANCISCO GOMEZ ALEMANY <zzemog99@gmail.com>
2021-03-19 19:18:01 +0100

    Project 2

---
+++ Assets/00-ADV Project 2/resetAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class resetAnimation : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("ResetAnimation", 21.0f);
+    }
+
+    void ResetAnimation()
+    {
+        SceneManager.LoadScene("Project 2");
+    }
+}
+
