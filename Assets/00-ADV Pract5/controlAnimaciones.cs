625dfa1e2d6edf68af330b49a1f2171fee4c8ad2
JOSE FRANCISCO GOMEZ ALEMANY <zzemog99@gmail.com>
2021-03-26 20:01:56 +0100

    ADV Pract 5

---
+++ Assets/00-ADV Pract5/controlAnimaciones.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class controlAnimaciones : MonoBehaviour
+{
+    public Animator animator;
+    public string booleanParameter;
+
+    bool b = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        b = !b;
+        animator.SetBool(booleanParameter, b);
+    }
+}
