b131112b9dc9d2b6a72964e6f2f0d07e426f7e6a
JOSE FRANCISCO GOMEZ ALEMANY <zzemog99@gmail.com>
2021-04-30 23:36:23 +0200

    Portfolio 8

---
+++ Assets/00-ADV Pract 8/IA_movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_movement : MonoBehaviour
+{
+    public Transform goal;
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator animator;
+    public Camera camara;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camara.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        animator.SetFloat("horizontal", transform.InverseTransformDirection(agent.velocity).x);
+        animator.SetFloat("vertical", transform.InverseTransformDirection(agent.velocity).z);
+    }
+}
